cfd3dfc0368fc580ea8b0df5f3e0a33ebc6c591f
sonykikuma <er.abhasoni@gmail.com>
2023-06-24 21:21:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const dateOfBirth = document.querySelector(".date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check-number");
+const outputEl= document.querySelector(".output")
+
+
+function compareValues(sum,luckyNumber){
+  if(sum%luckyNumber===0){
+outputEl.innerText= "Your birthday is lucky 🥳"
+
+    console.log("Your birthday is lucky 🥳")
+  } else {
+    outputEl.innerText= "Your birthday is not lucky  ☹️"
+    console.log("Your birthday is not lucky  ☹️")
+  }
+}
+
+
+
+const checkBdayIslucky=()=> {
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+
+if(sum && dob)
+
+    compareValues(sum,luckyNumber.value)
+else
+outputEl.innerText=" Please enter both the fields 😡"
+
+  }
+
+  function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum=0;
+for(let index = 0; index < dob.length; index++){
+  sum = sum + Number(dob.charAt(index));
+}
+return sum;
+  }
+  
+  
+  checkButton.addEventListener("click", checkBdayIslucky)
+  
